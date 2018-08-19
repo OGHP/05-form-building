4227aa56094c860f71b70db0f8610f839da021ac
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-08-18 19:07:16 -0700

    Initialize page, make a form, preview with handlebars, create a snippit that can be pasted into blogArticles.js, and style input to be gray

---
+++ heather-form-building/scripts/articleView.js
@@ -0,0 +1,136 @@
+'use strict';
+
+let articleView = {};
+
+articleView.populateFilters = () => {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      let val = $(this).find('address a').text();
+      let optionTag = `<option value="${val}">${val}</option>`;
+
+      if ($(`#author-filter option[value="${val}"]`).length === 0) {
+        $('#author-filter').append(optionTag);
+      }
+
+      val = $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+      if ($(`#category-filter option[value="${val}"]`).length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleAuthorFilter = () => {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-author="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = () => {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+articleView.handleMainNav = () => {
+  $('nav').on('click', '.tab', function(e) {
+    e.preventDefault();
+    $('.tab-content').hide();
+    $(`#${$(this).data('content')}`).fadeIn();
+  });
+
+  $('nav .tab:first').click();
+};
+
+articleView.setTeasers = () => {
+  $('.article-body *:nth-of-type(n+2)').hide();
+  $('article').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      },200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
+  });
+};
+
+// COMPLETED! COMMENT: Where is this function called? Why?
+// This is being called at the bottom of the new.html page so that it will run the js in the html page, to initialize the behaviors
+articleView.initNewArticlePage = () => {
+  // COMPLETED! Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').show();
+
+
+  // COMPLETED! The new articles we create will be copy/pasted into our source data file. Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+  $('#export-field').hide();
+
+  $('#article-json').on('focus', function(){
+    this.select();
+  });
+
+
+  // COMPLETED! Add an event handler to update the preview and the export field if any inputs change.
+  $('form').on('change', 'input, textarea', articleView.create);
+};
+
+articleView.create = () => {
+  // COMPLETED! Set up a var to hold new article we are creating.
+  let article = {};
+
+  // COMPLETED! Clear out #articles element, so we can put in the updated preview
+  $('#articles').html('');
+
+  // COMPLETED! Instantiate an article based on what's in the form fields:
+  article.title = $('#title').val();
+  article.author = $('#author').val();
+  article.authorUrl = $('#authorUrl').val();
+  article.category = $('#category').val();
+  article.publishedOn = $('#publishedOn').val();
+  article.body = $('#body').val();
+  let post = new Article(article);
+
+
+  // COMPLETED! Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').html( post.toHtml());
+
+
+
+  // STRETCH GOAL TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  //$('pre code').each();
+
+
+  // COMPLETED! Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#json-output').show();
+  $('#article-json').val(JSON.stringify(article) + ',');
+};
+
+// COMMENT: Where is this function called? Why?
+// This is called on the index.html page so that it will run the js in the html page, to initialize the behaviors
+articleView.initIndexPage = () => {
+  articles.forEach(article => $('#articles').append(article.toHtml()));
+  articleView.populateFilters();
+  articleView.handleCategoryFilter();
+  articleView.handleAuthorFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
+};
